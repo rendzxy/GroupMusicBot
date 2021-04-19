ef7073c6e3f37c22ef97a3c1b5567fc7d72d3312
xrrmli <80378609+xrrmli@users.noreply.github.com>
2021-04-19 08:48:11 +0700

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -20,7 +20,7 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {duration} minute(s)"
+            f"Maaf! Lagu yang berdurasi lebih lama dari **{DURATION_LIMIT} menit** tidak dapat diputar,\nkarena durasi lagu yang kamu minta **{audio.duration / 60} menit**"
         )
 
     ydl.download([url])
